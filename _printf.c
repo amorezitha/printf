a8c0be9fc0a22caeba3d779a7a807cb645747a9b
Owura-Kwaku <eoa524@gmail.com>
2023-06-28 01:27:19 -0700

    bug fixes

--- _printf.c
+++ _printf.c
@@ -7,6 +7,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format.
  * Return: Printed chars
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -51,11 +52,13 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
+
 /**
- * printed_buffer - prints the contents of the buffer if it exist
+ * print_buffer - prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
+
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
--- size.c
+++ size.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
@@ -6,6 +7,7 @@
  *
  * Return: Precision.
  */
+
 int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
@@ -13,9 +15,10 @@ int get_size(const char *format, int *i)
 
 	if (format[curr_i] == 'l')
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+
+	if (format[curr_i] == 'h')
 		size = S_SHORT;
-	
+
 	if (size == 0)
 		*i = curr_i - 1;
 	else
